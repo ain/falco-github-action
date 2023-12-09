72ee105d21659e0e406d0bac6c345e10a63ecfc2
Ain Tohvri <ain@flashbit.net>
2023-12-09 13:35:22 +0100

    Add entrypoint logic for options

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,18 @@
 
 set -e
 
-falco $1 $2 $3 $4
+include=""
+if [ -n $2 ]
+then
+  include="-I ${$2}"
+fi
+
+verbosity=""
+if [ $3 == "normal" ]
+then
+  verbosity="-v"
+elif [ $3 == "high" ]
+  verbosity="-vv"
+fi
+
+falco $1 $include $verbosity $4
